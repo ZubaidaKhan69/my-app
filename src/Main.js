26f09b0d3d35ded59124560d319f24c27b1c7414
ZubaidaKhan69 <zubaida.khan@umuzi.org>
2026-08-25 17:33:29 +0200

    Update App component

---
+++ src/Main.js
@@ -0,0 +1,10 @@
+function Main() {
+    return (
+        <main>
+            <h1>Welcome to Little Lemon</h1>
+            <p>Reserve a table and enjoy our Mediterranean cuisine!</p>
+        </main>
+    );
+}
+
+export default Main;
