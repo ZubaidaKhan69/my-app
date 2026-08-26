32b58e8f542829c1e64c704397b8692567679e61
ZubaidaKhan69 <zubaida.khan@umuzi.org>
2026-08-26 10:20:24 +0200

    Updated website

--- src/Nav.js
+++ src/Nav.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import logo from './Logo.svg'; // or your logo image name
+import {Link} from 'react-router-dom';
 
 function Nav() {
   return (
     <nav className="container nav-container">
       <img src={logo} alt="Little Lemon Logo" className="logo" />
       <ul className="nav-links">
-        <li><a href="/">Home</a></li>
-        <li><a href="/">About</a></li>
-        <li><a href="/">Menu</a></li>
-        <li><a href="/">Reservations</a></li>
-        <li><a href="/">Order Online</a></li>
-        <li><a href="/">Login</a></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/menu">Menu</Link></li>
+        <li><Link to="/reservations">Reservations</Link></li>
+        <li><Link to="/order">Order Online</Link></li>
+        <li><Link to="/login">Login</Link></li>
       </ul>
     </nav>
   );
